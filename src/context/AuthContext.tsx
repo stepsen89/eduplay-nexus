cff866f46912d86d5c8881524076f81e1fdc23a5
Steph <stepsen.dev@gmail.com>
2023-11-27 17:11:02 +0000

    ignore build errors

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,16 +1,17 @@
 "use client";
 import React from "react";
-import { onAuthStateChanged, getAuth } from "firebase/auth";
+import { onAuthStateChanged, getAuth, UserInfo } from "firebase/auth";
 import firebase_app from "@/firebase/config";
+import firebase from "firebase/compat/app";
 
 const auth = getAuth(firebase_app);
 
-export const AuthContext = React.createContext({});
+export const AuthContext = React.createContext<firebase.UserInfo | null>(null);
 
 export const useAuthContext = () => React.useContext(AuthContext);
 
-export const AuthContextProvider = ({ children }) => {
-  const [user, setUser] = React.useState(null);
+export const AuthContextProvider = ({ children }: { children: React.ReactNode }) => {
+  const [user, setUser] = React.useState<any | null>(null);
   const [loading, setLoading] = React.useState(true);
 
   React.useEffect(() => {
@@ -27,7 +28,7 @@ export const AuthContextProvider = ({ children }) => {
   }, []);
 
   return (
-    <AuthContext.Provider value={{ user }}>
+    <AuthContext.Provider value={user}>
       {loading ? <div>Loading...</div> : children}
     </AuthContext.Provider>
   );
